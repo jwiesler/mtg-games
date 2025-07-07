dbef6e1b077870968cf9e16ff58ed2ed3c4c82c7
Julian Wiesler <wiesleju@gmail.com>
2025-07-07 22:32:45 +0200

    DeckTable

---
+++ app/components/DeckTable.tsx
@@ -0,0 +1,53 @@
+import {
+  Link,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import type { Deck } from "~/generated/prisma/client";
+
+export function DeckTable({
+  decks,
+  usersMap,
+}: {
+  decks: Deck[];
+  usersMap: Record<string, string>;
+}) {
+  return (
+    <TableContainer component={Paper} sx={{ maxHeight: 400 }}>
+      <Table
+        sx={{ minWidth: 650 }}
+        aria-label="simple table"
+        stickyHeader={true}
+      >
+        <TableHead>
+          <TableRow>
+            <TableCell width={"3em"}>#</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Besitzer</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {decks.map((deck, index) => (
+            <TableRow
+              key={deck.name}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {index + 1}
+              </TableCell>
+              <TableCell>
+                <Link href={`/deck/${deck.id}`}>{deck.name}</Link>
+              </TableCell>
+              <TableCell>{usersMap[deck.ownerId]}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
