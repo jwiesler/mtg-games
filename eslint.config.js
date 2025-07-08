513f50f46f8642ed48d75317bc4eeed76adca926
Julian Wiesler <wiesleju@gmail.com>
2025-07-08 21:22:57 +0200

    Fix eslint

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config([
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    // Note: there should be no other properties in this object
+    ignores: ["**/build/*", ".react-router"],
+  },
+]);
