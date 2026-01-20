2e410f64ec39981e42320cd2d933f83e02721af2
Julian Wiesler <wiesleju@gmail.com>
2026-01-20 21:34:00 +0100

    test: Decks page tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
+  workers: 1,
   testDir: "./tests",
   timeout: 30_000,
-  retries: process.env.CI ? 2 : 0,
+  retries: /*process.env.CI ? 2 :*/ 0,
   use: {
     baseURL: "http://localhost:5173",
     trace: "on-first-retry",
