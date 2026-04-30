2f1f4482672094a0887c090888f8a7e6792e91be
Julian Wiesler <wiesleju@gmail.com>
2026-04-30 09:40:31 +0200

    ci: Increase playwright retries in ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   workers: 1,
   testDir: "./tests",
   timeout: 30_000,
-  retries: /*process.env.CI ? 2 :*/ 0,
+  retries: process.env.CI ? 2 : 0,
   use: {
     baseURL: "http://localhost:5173",
     trace: "on-first-retry",
