71e3219830d37260e04d8aedb45a3d1d05c39183
Julian Wiesler <wiesleju@gmail.com>
2026-05-02 11:19:59 +0200

    ci: Pre build server to prevent flaky playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig } from "@playwright/test";
 
+const port = 4173;
 export default defineConfig({
   workers: 1,
   testDir: "./tests",
   timeout: 30_000,
   retries: process.env.CI ? 2 : 0,
   use: {
-    baseURL: "http://localhost:5173",
+    baseURL: `http://localhost:${port}`,
     trace: "on-first-retry",
   },
   webServer: {
-    command: "npm run dev",
-    port: 5173,
+    command: `npx react-router build && npx vite preview --port ${port}`,
+    port: port,
     reuseExistingServer: !process.env.CI,
   },
 });
