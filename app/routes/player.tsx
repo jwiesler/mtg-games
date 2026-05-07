6c060cd26a83915cf4eb659f64397e80ab6d033e
Julian Wiesler <wiesleju@gmail.com>
2026-05-07 14:04:01 +0200

    feat: Charts for the player page

--- app/routes/player.tsx
+++ app/routes/player.tsx
@@ -1,4 +1,6 @@
 import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import {
   type LoaderFunctionArgs,
@@ -7,6 +9,9 @@ import {
 } from "react-router";
 
 import { parseIdParam } from "~/api.server";
+import { type Game, NamesChart } from "~/components/NamesChart";
+import { PlacingsChart } from "~/components/PlacingsChart";
+import PlayersChart from "~/components/PlayersChart";
 import RecentPlays from "~/components/RecentPlays";
 import prisma from "~/db.server";
 import { NotFound } from "~/responses.server";
@@ -70,21 +75,69 @@ export const meta: MetaFunction<typeof loader> = () => [
   },
 ];
 
+function Stats({ games, playerId }: { games: Game[]; playerId: number }) {
+  return (
+    <Card>
+      <CardContent
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          flexDirection: "row",
+          justifyContent: "space-around",
+        }}
+      >
+        <Box sx={{ textAlign: "center" }}>
+          <Typography variant="h5" gutterBottom>
+            Platzierungen
+          </Typography>
+          <PlacingsChart
+            filterBy="player"
+            filterById={playerId}
+            games={games}
+          />
+        </Box>
+        <Box sx={{ textAlign: "center" }}>
+          <Typography variant="h5" gutterBottom>
+            Spieleranzahl
+          </Typography>
+          <PlayersChart games={games} />
+        </Box>
+        <Box sx={{ textAlign: "center" }}>
+          <Typography variant="h5" gutterBottom>
+            Spieler
+          </Typography>
+          <NamesChart
+            filterBy="player"
+            filterById={playerId}
+            nameKey="deck"
+            games={games}
+          />
+        </Box>{" "}
+      </CardContent>
+      <div></div>
+    </Card>
+  );
+}
+
 export default function Player() {
   const { user, games } = useLoaderData<typeof loader>();
   return (
     <Box
       sx={{
-        my: 4,
+        gap: "1em",
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
-        alignItems: "center",
       }}
     >
-      <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+      <Typography
+        variant="h4"
+        component="h1"
+        sx={{ mb: 2, textAlign: "center" }}
+      >
         Spiele von {user.name}
       </Typography>
+      <Stats playerId={user.id} games={games} />
       <RecentPlays
         games={games}
         columnKey="deck"
