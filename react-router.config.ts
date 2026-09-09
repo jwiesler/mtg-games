5410d0ddc2142e0616efd0a5770625a37ced976b
Julian Wiesler <wiesleju@gmail.com>
2026-09-09 16:14:29 +0200

    chore: Formatting

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,7 +4,5 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-  allowedActionOrigins: [
-    "mtg.jwiesler.de"
-  ],
+  allowedActionOrigins: ["mtg.jwiesler.de"],
 } satisfies Config;
